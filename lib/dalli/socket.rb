c9f15e5848c2a3887e0b3c13b3747de673eb5b13
Falk Pauser <falk.pauser@gmail.com>
2015-03-17 15:05:40 +0100

    Changed os detection to RbConfig which works with jruby

--- lib/dalli/socket.rb
+++ lib/dalli/socket.rb
@@ -126,7 +126,7 @@ rescue LoadError
     end
   end
 
-  if RUBY_PLATFORM =~ /mingw|mswin/
+  if RbConfig::CONFIG['host_os'] =~ /mingw|mswin/
     class Dalli::Server::KSocket::UNIX
       def initialize(*args)
         raise Dalli::DalliError, "Unix sockets are not supported on Windows platform."
